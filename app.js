25d6e8e2ff43cf61159c70b619ac073887ed95ab
Julian Garritano <julian.garritano@radialpoint.com>
2015-10-04 19:19:08 -0400

    More improvements/API added

--- app.js
+++ app.js
@@ -42,10 +42,11 @@ storage.initSync();
 
 app.get('/users', function(req, res) {
     var storageValues = storage.values();
+
     storageValues = _.reject(storageValues, function(value) {
         return !value.appUserId;
     });
-    console.log('storage.values :: ', storageValues);
+
     res.json(storageValues);
 });
 
@@ -55,6 +56,10 @@ app.post('/webhook', function(req, res) {
     var userInfo = storage.getItem(STORAGE_PREFIX_USER + appUserId);
     var message = req.body.items[0].text;
     var role = req.body.items[0].role;
+
+    if (!appUserId) {
+        return res.status(400).end();
+    }
 
     if (role === 'appUser' && targetUser) {
         // Messages
@@ -70,11 +75,17 @@ app.post('/webhook', function(req, res) {
 });
 
 app.post('/login', function(req, res) {
-    storage.setItem(STORAGE_PREFIX_USER + req.body.appUserId, {
+    var appUserId = req.body.appUserId;
+
+    if (!appUserId) {
+        return res.status(400).end();
+    }
+
+    storage.setItem(STORAGE_PREFIX_USER + appUserId, {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        appUserId: req.body.appUserId
+        appUserId: appUserId
     }).then(function() {
         res.status(200).end();
     });
@@ -91,18 +102,71 @@ app.post('/connect', function(req, res) {
         storage.setItem(STORAGE_PREFIX_CONNECTION + appUserIdA, appUserIdB);
         storage.setItem(STORAGE_PREFIX_CONNECTION + appUserIdB, appUserIdA);
 
+        appUserA.connectedTo = appUserIdB;
+        appUserB.connectedTo = appUserIdA;
+        storage.setItem(STORAGE_PREFIX_USER + appUserIdA, appUserA);
+        storage.setItem(STORAGE_PREFIX_USER + appUserIdB, appUserB);
+
         SupportKit.messages.create(appUserIdA, {
-            text: 'You are now connected with ' + appUserB.firstName + ' ' + appUserB.lastName + '. Any messages you send will be delivered to them.',
+            text: 'You are now connected to ' + appUserB.firstName + ' ' + appUserB.lastName + '. Any messages you send will be delivered to them.',
         }, 'appMaker', jwt);
 
         SupportKit.messages.create(appUserIdB, {
-            text: 'You are now connected with ' + appUserA.firstName + ' ' + appUserA.lastName + '. Any messages you send will be delivered to them.',
+            text: 'You are now connected to ' + appUserA.firstName + ' ' + appUserA.lastName + '. Any messages you send will be delivered to them.',
         }, 'appMaker', jwt);
 
         res.status(201).end();
     }
 
     res.status(400).end();
+});
+
+app.post('/disconnect', function(req, res) {
+    var appUserIdA = req.body.appUserA;
+    var appUserIdB = req.body.appUserB;
+
+    if (storage.getItem(STORAGE_PREFIX_CONNECTION + appUserIdA) && storage.getItem(STORAGE_PREFIX_CONNECTION + appUserIdB)) {
+        var appUserA = storage.getItem(STORAGE_PREFIX_USER + appUserIdA);
+        var appUserB = storage.getItem(STORAGE_PREFIX_USER + appUserIdB);
+
+        storage.removeItem(STORAGE_PREFIX_CONNECTION + appUserIdA);
+        storage.removeItem(STORAGE_PREFIX_CONNECTION + appUserIdB);
+
+        delete appUserA.connectedTo;
+        delete appUserB.connectedTo;
+
+        storage.setItem(STORAGE_PREFIX_USER + appUserIdA, appUserA);
+        storage.setItem(STORAGE_PREFIX_USER + appUserIdB, appUserB);
+
+        SupportKit.messages.create(appUserIdA, {
+            text: 'You are no longer connected to ' + appUserB.firstName + ' ' + appUserB.lastName + '.',
+        }, 'appMaker', jwt);
+
+        SupportKit.messages.create(appUserIdB, {
+            text: 'You are no longer connected to ' + appUserA.firstName + ' ' + appUserA.lastName + '.',
+        }, 'appMaker', jwt);
+
+        res.status(201).end();
+    }
+
+    res.status(400).end();
+});
+
+app.post('/checkIn', function(req, res) {
+    var appUserId = req.body.appUserId;
+    var appUser = storage.getItem(STORAGE_PREFIX_USER + appUserId);
+
+    if (!appUserId) {
+        return res.status(400).end();
+    }
+
+    appUser.flickTitle = req.body.flickTitle;
+    appUser.remaining = req.body.remaining;
+    appUser.lastSeen = Date.now();
+
+    storage.setItem(STORAGE_PREFIX_USER + appUserId, appUser);
+
+    res.status(200).end();
 });
 
 app.listen(process.env.PORT || port);
